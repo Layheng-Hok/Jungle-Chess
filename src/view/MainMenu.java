3433eb7220bc806bd948cca31282c11b78da74f4
Hok Layheng <100819203+Layheng-Hok@users.noreply.github.com>
2023-05-01 18:27:52 +0700

    Update MainMenu.java

--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -7,7 +7,7 @@ public class MainMenu extends JFrame {
         this.setTitle("Jungle Chess (斗兽棋)");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
-        this.setSize(1250, 950);
+        this.setSize(1200, 900);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
         this.setLayout(null);
